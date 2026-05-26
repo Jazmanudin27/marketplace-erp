8a30dffe261e8d0fbfb856dce0cbb17f3aa74669
Jazmanudin27 <jazmanudin9@gmail.com>
2026-05-26 11:53:21 +0700

    update

--- app/Http/Controllers/MarketplaceSyncController.php
+++ app/Http/Controllers/MarketplaceSyncController.php
@@ -4,30 +4,61 @@ namespace App\Http\Controllers;
 
 use App\Models\MarketplaceAccount;
 use App\Services\Marketplace\MarketplaceManager;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class MarketplaceSyncController extends Controller
 {
+    protected $marketplaceManager;
+
+    public function __construct(MarketplaceManager $marketplaceManager)
+    {
+        $this->marketplaceManager = $marketplaceManager;
+    }
+
     public function syncProducts($id)
     {
         try {
 
             $account = MarketplaceAccount::findOrFail($id);
 
-            $service = MarketplaceManager::driver(
+            /*
+            |--------------------------------------------------------------------------
+            | Ambil Driver Marketplace
+            |--------------------------------------------------------------------------
+            */
+
+            $service = $this->marketplaceManager->driver(
                 $account->platform
             );
 
+            /*
+            |--------------------------------------------------------------------------
+            | Sync / Ambil Produk
+            |--------------------------------------------------------------------------
+            */
+
             $products = $service->getProducts($account);
 
-            dd($products);
+            return response()->json([
+                'success' => true,
+                'message' => 'Produk berhasil disinkronkan',
+                'data' => $products,
+            ]);
 
         } catch (\Exception $e) {
 
-            dd([
+            Log::error('Sync Product Error', [
                 'message' => $e->getMessage(),
                 'line' => $e->getLine(),
                 'file' => $e->getFile(),
             ]);
+
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage(),
+                'line' => $e->getLine(),
+            ], 500);
         }
     }
 }

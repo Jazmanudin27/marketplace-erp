856153b6aa8c09fa77557582b9fff5c94e78fc06
Jazmanudin27 <jazmanudin9@gmail.com>
2026-05-26 11:54:41 +0700

    update

--- app/Http/Controllers/MarketplaceSyncController.php
+++ app/Http/Controllers/MarketplaceSyncController.php
@@ -4,61 +4,31 @@ namespace App\Http\Controllers;
 
 use App\Models\MarketplaceAccount;
 use App\Services\Marketplace\MarketplaceManager;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
 class MarketplaceSyncController extends Controller
 {
-    protected $marketplaceManager;
-
-    public function __construct(MarketplaceManager $marketplaceManager)
-    {
-        $this->marketplaceManager = $marketplaceManager;
-    }
-
     public function syncProducts($id)
     {
-        try {
+        $account = MarketplaceAccount::findOrFail($id);
+        echo $account;
+        // try {
 
-            $account = MarketplaceAccount::findOrFail($id);
+        //     $account = MarketplaceAccount::findOrFail($id);
+        //     $service = MarketplaceManager::driver(
+        //         $account->platform
+        //     );
 
-            /*
-            |--------------------------------------------------------------------------
-            | Ambil Driver Marketplace
-            |--------------------------------------------------------------------------
-            */
+        //     $products = $service->getProducts($account);
 
-            $service = $this->marketplaceManager->driver(
-                $account->platform
-            );
+        //     dd($products);
 
-            /*
-            |--------------------------------------------------------------------------
-            | Sync / Ambil Produk
-            |--------------------------------------------------------------------------
-            */
+        // } catch (\Exception $e) {
 
-            $products = $service->getProducts($account);
-
-            return response()->json([
-                'success' => true,
-                'message' => 'Produk berhasil disinkronkan',
-                'data' => $products,
-            ]);
-
-        } catch (\Exception $e) {
-
-            Log::error('Sync Product Error', [
-                'message' => $e->getMessage(),
-                'line' => $e->getLine(),
-                'file' => $e->getFile(),
-            ]);
-
-            return response()->json([
-                'success' => false,
-                'message' => $e->getMessage(),
-                'line' => $e->getLine(),
-            ], 500);
-        }
+        //     dd([
+        //         'message' => $e->getMessage(),
+        //         'line' => $e->getLine(),
+        //         'file' => $e->getFile(),
+        //     ]);
+        // }
     }
 }

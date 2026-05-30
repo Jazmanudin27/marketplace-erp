dafc1fd9f3a73d78a0c5630afd046600e328e1b8
Jazmanudin27 <jazmanudin9@gmail.com>
2026-05-30 19:47:00 +0700

    update

--- app/Http/Controllers/MarketplaceSyncController.php
+++ app/Http/Controllers/MarketplaceSyncController.php
@@ -4,10 +4,40 @@ namespace App\Http\Controllers;
 
 use App\Models\MarketplaceAccount;
 use App\Services\Marketplace\MarketplaceManager;
+use Illuminate\Support\Facades\Http;
 
 class MarketplaceSyncController extends Controller
 {
+
     public function syncProducts($id)
+    {
+        $account = MarketplaceAccount::findOrFail($id);
+
+        $path = '/seller/202309/shops';
+
+        $service = new \App\Services\Marketplace\TikTok\TikTokService();
+
+        $params = [
+            'app_key' => config('services.tiktok.app_key'),
+            'timestamp' => time(),
+        ];
+
+        $params['sign'] = $service->sign($path, $params);
+
+        $response = Http::withHeaders([
+            'x-tts-access-token' => $account->access_token,
+            'Content-Type' => 'application/json',
+        ])->get(
+            'https://open-api.tiktokglobalshop.com' . $path,
+            $params
+        );
+
+        dd([
+            'status' => $response->status(),
+            'body' => $response->json(),
+        ]);
+    }
+    public function syncProductss($id)
     {
 
         try {

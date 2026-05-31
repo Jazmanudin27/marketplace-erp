0ef6415e5477bbb5863012e4f8c4f363481f1b7a
Jazmanudin27 <jazmanudin9@gmail.com>
2026-06-01 06:17:30 +0700

    update

--- app/Http/Controllers/MarketplaceSyncController.php
+++ app/Http/Controllers/MarketplaceSyncController.php
@@ -94,6 +94,8 @@ class MarketplaceSyncController extends Controller
             }
 
             $rawOrders = $service->getOrders($account);
+
+            dd($rawOrders);
             $syncedCount = 0;
             $skippedCount = 0;
 

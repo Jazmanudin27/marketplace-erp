d8e6e33d57f51c280d450aae847f0f4dfdde5b8b
Jazmanudin27 <jazmanudin9@gmail.com>
2026-06-01 12:28:33 +0700

    update

--- app/Services/Marketplace/TikTok/TikTokService.php
+++ app/Services/Marketplace/TikTok/TikTokService.php
@@ -208,8 +208,8 @@ class TikTokService
             'total_count' => $result['data']['total_count'] ?? 0,
             'returned_orders' => count($result['data']['orders'] ?? []),
             'next_page_token' => $result['data']['next_page_token'] ?? null,
-            'from' => date('Y-m-d H:i:s', $body['create_time_ge']),
-            'to' => date('Y-m-d H:i:s', $body['create_time_lt']),
+            'from' => $body['create_time_ge'] ? date('Y-m-d H:i:s', $body['create_time_ge']) : null,
+            'to' => $body['create_time_lt'] ? date('Y-m-d H:i:s', $body['create_time_lt']) : null,
         ]);
         // dd([
         //     'status' => $response->status(),
